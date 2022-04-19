d6bb296672bbabd28c06fbc461f024945a828c56
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-04-19 19:10:22 +0300

    Подготовил модель

---
+++ PersonalList/DataManager.swift
@@ -0,0 +1,40 @@
+//
+//  DataManager.swift
+//  PersonalList
+//
+//  Created by Василий Полторак on 19.04.2022.
+//
+
+class DataManager {
+    let names = [
+        "Glen",
+        "Tom",
+        "Edward",
+        "Willy",
+        "Jack"
+    ]
+    
+    let lastNames = [
+        "Lantz",
+        "Hanson",
+        "Scissorhands",
+        "Wonka",
+        "Sparrow"
+    ]
+    
+    let phones = [
+        "8-111-111-11-11",
+        "8-222-222-22-22",
+        "8-333-333-33-33",
+        "8-444-444-44-44",
+        "8-555-555-55-55",
+    ]
+    
+    let mails = [
+        "1111@mail.ru",
+        "2222@mail.ru",
+        "3333@mail.ru",
+        "4444@mail.ru",
+        "5555@mail.ru"
+    ]
+}
---
+++ PersonalList/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  PersonalList
+//
+//  Created by Василий Полторак on 19.04.2022.
+//
+
+struct Person {
+    let name: String
+    let lastName: String
+    let phone: String
+    let mail: String
+}
